e56bdf1217526d10dd5619c70b45cb97a01aefa9
이성준 <qoik11@naver.com>
2019-09-17 23:13:42 +0900

    Logic

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {Alert} from 'react-native';
+import Loading from './Loading';
+import * as Location from 'expo-location';
+import axios from "axios";
+
+const API_KEY = "32ffac1a847e670d20b30159ebe0e34d";
+export default class extends React.Component{
+  state = {
+    isLoading : true
+  };
+  getWeather = async(latitude,longitude) => {  
+      const {data} = await axios.get(
+        `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}
+        &lon=${longitude}&APPID=${API_KEY}`)
+        console.log(data);
+      
+      
+  } ;
+  getLocation = async () => {
+    try {
+       await Location.requestPermissionsAsync();
+      const {
+        coords: {latitude,longitude}
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude,longitude);
+      this.setState({isLoading: false});
+
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
+    
+  }
+  componentDidMount(){
+    this.getLocation();
+  }
+  render(){
+    const { isLoading} = this.state;
+    return isLoading ?<Loading /> : null; 
+  }
+}
+
+
---
+++ README.md
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+# movieql
+=======
+# weather-app
+Learning React Native App
+>>>>>>> 92d3b97d8e92267d0577026459db846fcd552361
