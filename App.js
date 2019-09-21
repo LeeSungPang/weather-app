79c8f94f4c6c302371a30177fc3729bf11289f49
이성준 <qoik11@naver.com>
2019-09-21 16:12:11 +0900

    finish project

--- App.js
+++ App.js
@@ -37,6 +37,7 @@ export default class extends React.Component {
       Alert.alert("Can't find you.", "So sad");
     }
   };
+
   componentDidMount() {
     this.getLocation();
   }
--- Loading.js
+++ Loading.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import {StyleSheet,Text,View} from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
 
- export default function Loading(){
-    return (
-    <View style={styles.container} >
-    <Text style={styles.text} >리엑트 네이티브 앱</Text>
+export default function Loading() {
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <Text style={styles.text}>리엑트 네이티브 앱</Text>
     </View>
-    )
-};
+  );
+}
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent: "flex-end",
-        paddingHorizontal:30,
-        paddingVertical: 100,
-        backgroundColor: "#FDF6AA"
-    },
-    text :{
-        color:"#2c2c2c",
-        fontSize: 30
-
-    }
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingHorizontal: 30,
+    paddingVertical: 100,
+    backgroundColor: "#FDF6AA"
+  },
+  text: {
+    color: "#2c2c2c",
+    fontSize: 30
+  }
 });
